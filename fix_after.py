fb5a2ba7d02dfb27a88522008d460017b4e35fbf
koliskos <77557421+koliskos@users.noreply.github.com>
2022-04-12 13:01:29 -0400

    final fix_after. may edit to combine w/ sarah's
    this outputs a csv that is useable and has a different column for each of the 10 features of an applicant and each of the 5 features of a property. skylar has to look at sarah's version on collab. she'll probably edit this version to include parts of sarah's because sarah's uses good coding practices, which is something this code may be lacking in.

--- fix_after.py
+++ fix_after.py
@@ -1,25 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
+
 import pandas as pd
 import random
 import csv
-import os
-from ast import literal_eval
 
 
 
 fil = 'https://raw.githubusercontent.com/koliskos/exp1AdversarialInput/main/r5.csv'
 df = pd.read_csv(fil)
 num_rows = df.shape[0]
-#we know there are 5 columns
-# arr = np.empty((num_rows,5), dtype=float)
 arr_of_full_apps=[]
-#arr_of_full_apps=[['App_Number'], ['App_Feature_0'],['App_Feature_1'],['App_Feature_2'],['App_Feature_3'],['App_Feature_4'],['App_Feature_5'],['App_Feature_6'],['App_Feature_7'],['App_Feature_8'],['App_Feature_9'],['Prop_Feature_0'],['Prop_Feature_1'],['Prop_Feature_2'],['Prop_Feature_3'],['Prop_Feature_4'], ['Lender'], ['Approval_Status']]
-
-
-
-
 for i in range(num_rows):
     numApp=df.iat[i,0]
     appInfo=df.iat[i,1]
@@ -40,34 +31,23 @@ for i in range(num_rows):
     for i in range(len(appInfo_as_float)):
         ele=appInfo_as_float[i]
         print("ele is "+str(ele))
-        # ele.replace(" ", "")
-        # print (type(ele))
         if ele[0] == '-':
             eleNeg = ele[1:]
-            # print(ele)
-            # print(eleNeg)
             appNum = float(eleNeg)*-1
-            # print(type(now))
             appFloatsArray.append(appNum)
         else:
             appNum = float(ele)
             appFloatsArray.append(appNum)
         counter+=1
-    # print(nowFloatsArray)
 
 
     propLen =len(propInfo)
     propInfo= propInfo[1: propLen-1]
     propInfo_as_float = propInfo.split()
     for ele in propInfo_as_float:
-        # ele.replace(" ", "")
-        # print (type(ele))
         if ele[0] == '-':
             eleNeg = ele[1:]
-            # print(ele)
-            # print(eleNeg)
             now = float(eleNeg)*-1
-            # print(type(now))
             propertyAsArr.append(now)
         else:
             now = float(ele)
@@ -82,10 +62,4 @@ print(arr_of_full_apps)
 df_clean = pd.DataFrame (arr_of_full_apps, columns = ['App_Number', 'App_Feature_0','App_Feature_1','App_Feature_2','App_Feature_3','App_Feature_4','App_Feature_5','App_Feature_6','App_Feature_7','App_Feature_8','App_Feature_9','Prop_Feature_0','Prop_Feature_1','Prop_Feature_2','Prop_Feature_3','Prop_Feature_4', 'Lender', 'Approval_Status']
 )
 print (df_clean)
-
-    #     row_1_as_int.append(-1*float(eleNeg))
-    # else:
-    #     row_1_as_int.append(float(ele))
-
-
-# print(row_1_as_int)
+df_clean.to_csv('fix_after_output.csv')
