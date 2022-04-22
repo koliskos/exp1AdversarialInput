7d491c717766f00f471a03ced6f1c8c528dbfce7
koliskos <77557421+koliskos@users.noreply.github.com>
2022-04-22 14:54:51 -0400

    script to method-based

--- exp1_syntheticdata_logregmodel.py
+++ exp1_syntheticdata_logregmodel.py
@@ -9,11 +9,37 @@ import pandas as pd
 import random
 import csv
 
-df_clean = pd.read_csv('fix_after_output.csv')
+# df_clean = pd.read_csv('fix_after_output.csv')
 
-X_numpy = df_clean.drop("App_Number", axis=1)
-X = X_numpy.drop("Approval_Status", axis=1)
-y = df_clean[['Approval_Status']]
+def train_model(X_train, X_test, y_train,y_test):
+  # instantiate the model (using the default parameters)
+  model1 = LogisticRegression(max_iter=1000)
+  # fit the model with data
+  model1.fit(X_train,y_train.values.ravel())#y_train.values.ravel() because a 1d array is expected, not a column
+  print(y_train)
+  return model1
+
+def get_train_acc(model, X_train, y_train):
+  train_acc = model1.score(X_train, y_train)
+  print("The Accuracy for Training Set is {}".format(train_acc*100))
+  return train_acc
+
+def get_test_acc(model, X_test, y_test):
+  test_acc = model1.score(X_test, y_test)
+  print("The Accuracy for Testing Set is {}".format(test_acc*100))
+  return test_acc
+
+def cnf_matrix(model,X_test,y_test):
+  y_pred=model.predict(X_test)
+  cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+  print(cnf_matrix)
+  return cnf_matrix
+
+
+
+  
+  
+
 
 
 #**************************************************************************************************************************
@@ -25,34 +51,3 @@ y = df_clean[['Approval_Status']]
 
 
 
-# instantiate the model (using the default parameters)
-model1 = LogisticRegression(max_iter=1000)
-X_train, X_test, y_train, y_test = train_test_split(X, y) #nicely splits up data into training and testing data. We could stratify using y so that the random split of training/test data has same proportions of approval or rejection for Approval_Status
-
-# fit the model with data
-model1.fit(X_train,y_train.values.ravel())#y_train.values.ravel() because a 1d array is expected, not a column
-print(y_train)
-y_pred=model1.predict(X_test)
-
-cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
-# print(cnf_matrix)
-
-class_names=[0,1] # name  of classes
-fig, ax = plt.subplots()
-tick_marks = np.arange(len(class_names))
-plt.xticks(tick_marks, class_names)
-plt.yticks(tick_marks, class_names)
-# create heatmap
-sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap="YlGnBu" ,fmt='g')
-ax.xaxis.set_label_position("top")
-plt.tight_layout()
-plt.title('Confusion matrix', y=1.1)
-plt.ylabel('Actual label')
-plt.xlabel('Predicted label')
-# plt.show()
-train_acc = model1.score(X_train, y_train)
-print("The Accuracy for Training Set is {}".format(train_acc*100))
-test_acc = model1.score(X_test, y_test)
-print("The Accuracy for Testing Set is {}".format(test_acc*100))
-# scores = cr s_val_score(model1, X_train, y_train.values.ravel(), cv=10)
-# print('Cross-Validation Accuracy Scores', scores)
